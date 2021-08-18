a68946154a2cca775a4dfd26bed3e2636496cf6e
Atlas <jack@bentsen.us>
2021-08-18 14:19:04 -0700

    commented out unused line

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static-cache-v2";
+const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
@@ -6,7 +6,7 @@ const FILES_TO_CACHE = [
     '/index.html',
     '/index.js',
     '/db.js',
-    '/style.css',
+    '/styles.css',
     '/manifest.webmanifest',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png'
@@ -14,11 +14,13 @@ const FILES_TO_CACHE = [
 
 // Install
 self.addEventListener("install", function(evt){
+    // evt.waitUntil(
+    //     caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+    // );
     evt.waitUntil(
-        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
-    );
-    evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(FILES_TO_CACHE)
+        })
     );
 
     self.skipWaiting();
